c2351e4a37ced78ad89c321c46cb98071af3e028
apple <apple@apples-Air.lan>
2019-03-15 18:31:40 +0800

    +右侧素材列表素材分类显示，点击类型切换

--- unityProject/Assets/Scripts/Manager/GameManager/GameManager.cs
+++ unityProject/Assets/Scripts/Manager/GameManager/GameManager.cs
@@ -8,35 +8,42 @@ namespace GameMgr
         public List<string> homePathList = new List<string>();//选择界面的资源路径
         public List<string> drawBgPathList = new List<string>();//画图背景资源路径
 
-        public List<string> letterEyePathList = new List<string>();//字母眼睛资源路径
-        public List<string> numEyePathList = new List<string>();//数字眼睛资源路径
-        public List<string> aniEyePathList = new List<string>();//动物眼睛资源路径
+        private List<string> colorPathList = new List<string>();//颜色资源路径
 
-        public List<string> letterMouthPathList = new List<string>();//字母嘴巴资源路径
-        public List<string> numMouthPathList = new List<string>();//数字嘴巴资源路径
-        public List<string> aniMouthPathList = new List<string>();//动物嘴巴资源路径
+        private List<string> letterEyePathList = new List<string>();//字母眼睛资源路径
+        private List<string> numEyePathList = new List<string>();//数字眼睛资源路径
+        private List<string> aniEyePathList = new List<string>();//动物眼睛资源路径
 
-        public List<string> letterHairPathList = new List<string>();//字母头发资源路径
+        private List<string> letterMouthPathList = new List<string>();//字母嘴巴资源路径
+        private List<string> numMouthPathList = new List<string>();//数字嘴巴资源路径
+        private List<string> aniMouthPathList = new List<string>();//动物嘴巴资源路径
 
-        public List<string> letterHatPathList = new List<string>();//字母帽子资源路径
-        public List<string> numHatPathList = new List<string>();//数字帽子资源路径
-        public List<string> aniHatPathList = new List<string>();//动物帽子资源路径
+        private List<string> letterHairPathList = new List<string>();//字母头发资源路径
 
-        public List<string> letterHeadWarePathList = new List<string>();//字母饰品资源路径
-        public List<string> numHeadWarePathList = new List<string>();//数字饰品资源路径
+        private List<string> letterHatPathList = new List<string>();//字母帽子资源路径
+        private List<string> numHatPathList = new List<string>();//数字帽子资源路径
+        private List<string> aniHatPathList = new List<string>();//动物帽子资源路径
 
-        public List<string> letterHandPathList = new List<string>();//字母饰品资源路径
-        public List<string> numHandPathList = new List<string>();//数字饰品资源路径
-        public List<string> AniHandPathList = new List<string>();//数字饰品资源路径
+        private List<string> letterHeadWarePathList = new List<string>();//字母饰品资源路径
+        private List<string> numHeadWarePathList = new List<string>();//数字饰品资源路径
 
-        public List<string> letterLegPathList = new List<string>();//数字饰品资源路径
-        public List<string> numLegPathList = new List<string>();//数字饰品资源路径
-        public List<string> aniLegPathList = new List<string>();//数字饰品资源路径
+        private List<string> letterHandPathList = new List<string>();//字母饰品资源路径
+        private List<string> numHandPathList = new List<string>();//数字饰品资源路径
+        private List<string> AniHandPathList = new List<string>();//数字饰品资源路径
+
+        private List<string> letterLegPathList = new List<string>();//数字饰品资源路径
+        private List<string> numLegPathList = new List<string>();//数字饰品资源路径
+        private List<string> aniLegPathList = new List<string>();//数字饰品资源路径
+
+        public List<List<string>> resPathList = new List<List<string>>();//全部资源路径
+
+        public List<string> resPrefabPathList = new List<string>();//素材资源prefab路径
 
         public int homeSelectIndex = 0;//选择了那个字母或数字，存放的是下标
+        public int resTypeCount = 8;//素材资源种类，目前8种
         public Transform Root;
 
-        void Awake() 
+        void Awake()
         {
             instance = this;
             Root = GameObject.Find("root").GetComponent<RectTransform>();
@@ -44,11 +51,12 @@ namespace GameMgr
 
         void Start()
         {
-            InitItemList();
-
+            InitItemList();//首页选字母，数字
+            InitResPrefabList();//素材中的预制体路径
+            InitResList();//素材资源
         }
 
-        //初始化item列表
+        //初始化item列表,首页选字母的
         void InitItemList()
         {
             //26个字母
@@ -71,25 +79,38 @@ namespace GameMgr
             }
         }
 
+        //初始化素材列表
         void InitResList()
         {
+            //颜色
+            string colorPath = "sprite/small_fodder/color|color_{0}_icon";
+            for (int i = 0; i < 16; i++)
+            {
+                colorPathList.Add(string.Format(colorPath, i.ToString()));
+            }
             //眼睛
             string letterEyePath = "sprite/fodder/eye|eye_L_{0}";
+            string letterEyePath_R = "sprite/fodder/eye|eye_R_{0}";
             for (int i = 0; i < 25; i++)
             {
                 letterEyePathList.Add(string.Format(letterEyePath, i.ToString()));
+                letterEyePathList.Add(string.Format(letterEyePath_R, i.ToString()));
             }
 
             string numEyePath = "sprite/fodder/eye|number_eye_L_{0}";
-            for (int i = 0; i < 3; i++)
+            string numEyePath_R = "sprite/fodder/eye|number_eye_R_{0}";
+            for (int i = 0; i < 2; i++)
             {
                 numEyePathList.Add(string.Format(numEyePath, i.ToString()));
+                numEyePathList.Add(string.Format(numEyePath_R, i.ToString()));
             }
 
             string aniEyePath = "sprite/fodder/eye|animal_eye_L_{0}";
-            for (int i = 0; i < 4; i++)
+            string aniEyePath_R = "sprite/fodder/eye|animal_eye_R_{0}";
+            for (int i = 0; i < 3; i++)
             {
                 aniEyePathList.Add(string.Format(aniEyePath, i.ToString()));
+                aniEyePathList.Add(string.Format(aniEyePath_R, i.ToString()));
             }
 
             //嘴巴
@@ -100,13 +121,13 @@ namespace GameMgr
             }
 
             string numMouthPath = "sprite/fodder/mouth|number_mouse_{0}";
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 2; i++)
             {
                 numMouthPathList.Add(string.Format(numMouthPath, i.ToString()));
             }
 
             string aniMouthPath = "sprite/fodder/mouth|animal_mouse_{0}";
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 3; i++)
             {
                 aniMouthPathList.Add(string.Format(aniMouthPath, i.ToString()));
             }
@@ -115,7 +136,7 @@ namespace GameMgr
             string letterHairPath = "sprite/fodder/hair|hair_{0}";
             for (int i = 0; i < 10; i++)
             {
-                letterHatPathList.Add(string.Format(letterHairPath, i.ToString()));
+                letterHairPathList.Add(string.Format(letterHairPath, i.ToString()));
             }
 
             //帽子
@@ -144,50 +165,177 @@ namespace GameMgr
                 letterHeadWarePathList.Add(string.Format(letterHeadWearPath, i.ToString()));
             }
 
-            string numHeadWearPath = "sprite/fodder/headwear|number_headwear_0{0}";
-            for (int i = 0; i < 3; i++)
+            string numHeadWearPath = "sprite/fodder/headwear|number_headwear_{0}";
+            for (int i = 0; i < 2; i++)
             {
                 numHeadWarePathList.Add(string.Format(numHeadWearPath, i.ToString()));
             }
 
             //手
             string letterHandPath = "sprite/fodder/hand|hand_L_{0}";
+            string letterHandPath_R = "sprite/fodder/hand|hand_R_{0}";
             for (int i = 0; i < 26; i++)
             {
                 letterHandPathList.Add(string.Format(letterHandPath, i.ToString()));
+                letterHandPathList.Add(string.Format(letterHandPath_R, i.ToString()));
+
             }
 
             string numHandPath = "sprite/fodder/hand|number_hand_L_{0}";
-            for (int i = 0; i < 4; i++)
+            string numHandPath_R = "sprite/fodder/hand|number_hand_R_{0}";
+            for (int i = 0; i < 3; i++)
             {
                 numHandPathList.Add(string.Format(numHandPath, i.ToString()));
+                numHandPathList.Add(string.Format(numHandPath_R, i.ToString()));
             }
 
             string aniHandPath = "sprite/fodder/hand|animal_hand_L_{0}";
-            for (int i = 0; i < 4; i++)
+            string aniHandPath_R = "sprite/fodder/hand|animal_hand_R_{0}";
+            for (int i = 0; i < 3; i++)
             {
                 AniHandPathList.Add(string.Format(aniHandPath, i.ToString()));
+                AniHandPathList.Add(string.Format(aniHandPath_R, i.ToString()));
             }
 
             //脚
-            string letterLegPath = "sprite/fodder/leg/hat|leg_L_{0}";
+            string letterLegPath = "sprite/fodder/leg|leg_L_{0}";
+            string letterLegPath_R = "sprite/fodder/leg|leg_R_{0}";
             for (int i = 0; i < 26; i++)
             {
                 letterLegPathList.Add(string.Format(letterLegPath, i.ToString()));
+                letterLegPathList.Add(string.Format(letterLegPath_R, i.ToString()));
             }
 
             string numLegPath = "sprite/fodder/leg|number_leg_L_{0}";
-            for (int i = 0; i < 4; i++)
+            string numLegPath_R = "sprite/fodder/leg|number_leg_R_{0}";
+            for (int i = 0; i < 3; i++)
             {
                 numLegPathList.Add(string.Format(numLegPath, i.ToString()));
+                numLegPathList.Add(string.Format(numLegPath_R, i.ToString()));
             }
 
             string aniLegPath = "sprite/fodder/leg|animal_leg_L_{0}";
-            for (int i = 0; i < 4; i++)
+            string aniLegPath_R = "sprite/fodder/leg|animal_leg_R_{0}";
+            for (int i = 0; i < 3; i++)
             {
                 aniLegPathList.Add(string.Format(aniLegPath, i.ToString()));
+                aniLegPathList.Add(string.Format(aniLegPath_R, i.ToString()));
+            }
+
+            //组装
+
+            //颜色
+            resPathList.Add(colorPathList);
+            //眼睛
+            List<string> eyePathList = new List<string>();
+            for (int i = 0; i < letterEyePathList.Count; i++)
+            {
+                eyePathList.Add(letterEyePathList[i]);
+            }
+            for (int i = 0; i < numEyePathList.Count; i++)
+            {
+                eyePathList.Add(numEyePathList[i]);
+            }
+            for (int i = 0; i < aniEyePathList.Count; i++)
+            {
+                eyePathList.Add(aniEyePathList[i]);
+            }
+            resPathList.Add(eyePathList);
+            //嘴巴
+            List<string> mouthPathList = new List<string>();
+            for (int i = 0; i < letterMouthPathList.Count; i++)
+            {
+                mouthPathList.Add(letterMouthPathList[i]);
+            }
+            for (int i = 0; i < numMouthPathList.Count; i++)
+            {
+                mouthPathList.Add(numMouthPathList[i]);
+            }
+            for (int i = 0; i < aniMouthPathList.Count; i++)
+            {
+                mouthPathList.Add(aniMouthPathList[i]);
+            }
+            resPathList.Add(mouthPathList);
+            //头发
+            List<string> hairPathList = new List<string>();
+            for (int i = 0; i < letterHairPathList.Count; i++)
+            {
+                hairPathList.Add(letterHairPathList[i]);
+            }
+            resPathList.Add(hairPathList);
+            //帽子
+            List<string> hatPathList = new List<string>();
+            for (int i = 0; i < letterHatPathList.Count; i++)
+            {
+                hatPathList.Add(letterHatPathList[i]);
+            }
+            for (int i = 0; i < numHatPathList.Count; i++)
+            {
+                hatPathList.Add(numHatPathList[i]);
+            }
+            for (int i = 0; i < aniHatPathList.Count; i++)
+            {
+                hatPathList.Add(AniHandPathList[i]);
+            }
+            resPathList.Add(hatPathList);
+            //饰品
+            List<string> headWearPathList = new List<string>();
+            for (int i = 0; i < letterHeadWarePathList.Count; i++)
+            {
+                headWearPathList.Add(letterHeadWarePathList[i]);
+            }
+            for (int i = 0; i < numHeadWarePathList.Count; i++)
+            {
+                headWearPathList.Add(numHeadWarePathList[i]);
+            }
+            resPathList.Add(headWearPathList);
+            //手
+            List<string> handPathList = new List<string>();
+            for (int i = 0; i < letterHandPathList.Count; i++)
+            {
+                handPathList.Add(letterHandPathList[i]);
+            }
+            for (int i = 0; i < numHandPathList.Count; i++)
+            {
+                handPathList.Add(numHandPathList[i]);
+            }
+            for (int i = 0; i < AniHandPathList.Count; i++)
+            {
+                handPathList.Add(AniHandPathList[i]);
+            }
+            resPathList.Add(handPathList);
+            //脚
+            List<string> legPathList = new List<string>();
+            for (int i = 0; i < letterLegPathList.Count; i++)
+            {
+                legPathList.Add(letterLegPathList[i]);
+            }
+            for (int i = 0; i < numLegPathList.Count; i++)
+            {
+                legPathList.Add(numLegPathList[i]);
+            }
+            for (int i = 0; i < aniLegPathList.Count; i++)
+            {
+                legPathList.Add(aniLegPathList[i]);
+            }
+            resPathList.Add(legPathList);
+        }
+
+        void InitResPrefabList()
+        {
+            string path = "prefabs/join|res_obj_{0}";
+            for (int i = 0; i < resTypeCount; i++)
+            {
+                resPrefabPathList.Add(string.Format(path, i.ToString()));
             }
         }
+
+        public string FodderToSamllFodderPath(string path)
+        {
+            string tmpPath = path.Replace("fodder", "small_fodder");
+            return tmpPath + "_icon";
+        }
     }
-    
+
+   
 }
--- unityProject/Assets/Scripts/UI/Join/JoinMainView.cs
+++ unityProject/Assets/Scripts/UI/Join/JoinMainView.cs
@@ -13,6 +13,9 @@ public class JoinMainView : MonoBehaviour
     public Button BtnOk;
     public Image ImgReference;
     public Image ImgBody;
+    public List<GameObject> ResScrollViewList;//各类素材列表
+    public List<Transform> ResContentList;//各类素材容器-父节点
+
     private List<Transform> typeTransList=new List<Transform>();//类型列表
     public Transform conResType;//类型列表的父节点
 
@@ -26,6 +29,7 @@ public class JoinMainView : MonoBehaviour
         Init();
         TypeButtonClick(0);//初始选中第一个类型
         ShowTypeByStep(step);
+        LoadAllResList();
     }
 
 
@@ -38,13 +42,12 @@ public class JoinMainView : MonoBehaviour
         UIHelper.instance.SetImage(GameManager.instance.drawBgPathList[GameManager.instance.homeSelectIndex], ImgBody, true);
 
         //左下角参考缩略图
-        Debug.Log(GameManager.instance.homeSelectIndex);
         UIHelper.instance.SetImage(GameManager.instance.homePathList[GameManager.instance.homeSelectIndex], ImgReference, true);
 
         //加载所有类型
         string typeUnSelectPath = "sprite/ui|splice_type_{0}";
         string typeSelectPath = "sprite/ui|splice_type_{0}_select";
-        for (int i = 0; i < typeCount; i++)
+        for (int i = 0; i < GameManager.instance.resTypeCount; i++)
         {
             int clickType = i;
             Transform t = UIHelper.instance.LoadPrefab("prefabs/join|res_type_item", conResType,Vector3.zero,Vector3.one,false).transform;
@@ -81,17 +84,20 @@ public class JoinMainView : MonoBehaviour
 
     private void TypeButtonClick(int n)
     {
-        for (int i = 0; i < typeCount; i++)
+        cur_type = n;
+        for (int i = 0; i < GameManager.instance.resTypeCount; i++)
         {
             if (i==n)
             {
                 typeTransList[i].GetChild(0).gameObject.SetActive(true);
                 typeTransList[i].GetChild(1).gameObject.SetActive(false);
+                ResScrollViewList[i].gameObject.SetActive(true);
             }
             else
             {
                 typeTransList[i].GetChild(0).gameObject.SetActive(false);
                 typeTransList[i].GetChild(1).gameObject.SetActive(true);
+                ResScrollViewList[i].gameObject.SetActive(false);
             }
         }
     }
@@ -105,7 +111,7 @@ public class JoinMainView : MonoBehaviour
             BtnNext.gameObject.SetActive(true);
             BtnOk.gameObject.SetActive(false);
             typeTransList[0].gameObject.SetActive(true);
-            for (int i = 1; i < typeCount; i++)
+            for (int i = 1; i < GameManager.instance.resTypeCount; i++)
             {
                 typeTransList[i].gameObject.SetActive(false);
             }
@@ -119,7 +125,7 @@ public class JoinMainView : MonoBehaviour
             {
                 typeTransList[i].gameObject.SetActive(true);
             }
-            for (int i = 4; i < typeCount; i++)
+            for (int i = 4; i < GameManager.instance.resTypeCount; i++)
             {
                 typeTransList[i].gameObject.SetActive(false);
             }
@@ -154,11 +160,42 @@ public class JoinMainView : MonoBehaviour
             {
                 typeTransList[i].gameObject.SetActive(false);
             }
-            for (int i = 6; i < typeCount; i++)
+            for (int i = 6; i < GameManager.instance.resTypeCount; i++)
             {
                 typeTransList[i].gameObject.SetActive(true);
             }
         }
         TypeButtonClick(cur_type);
+    }
+
+    private void LoadAllResList()
+    {
+        for (int i = 0; i < GameManager.instance.resTypeCount; i++)
+        {
+            string resPrefabPath = GameManager.instance.resPrefabPathList[i];
+            List<string> resPath = GameManager.instance.resPathList[i];
+            if (resPath.Count <= 0)
+            {
+                continue;
+            }
+            for (int j = 0; j < resPath.Count; j++)
+            {
+                GameObject resObj = UIHelper.instance.LoadPrefab(resPrefabPath, ResContentList[i], Vector3.zero, Vector3.one, false);
+                if (i == 0 || i == 1 || i == 6 || i== 7)
+                {
+                    string imgPath = resPath[j];
+                    if (i != 0)
+                    {
+                        imgPath = GameManager.instance.FodderToSamllFodderPath(resPath[j]);
+                    }
+                    UIHelper.instance.SetImage(imgPath, resObj.transform.Find("img_res").GetComponent<Image>(), true);
+                }
+                else
+                {
+                    string imgPath  = GameManager.instance.FodderToSamllFodderPath(resPath[j]);
+                    UIHelper.instance.SetImage(imgPath, resObj.transform.GetComponent<Image>(), true);
+                }
+            }
+        }
     }
 }
