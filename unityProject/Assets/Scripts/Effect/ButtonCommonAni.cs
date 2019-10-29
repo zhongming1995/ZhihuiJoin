04508baaa3950b2582e4a5690f677b54c7be352f
zhongming1995 <17621940295@163.com>
2019-10-29 16:51:19 +0800

    +-button效果

---
+++ unityProject/Assets/Scripts/Effect/ButtonCommonAni.cs
@@ -0,0 +1,30 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonCommonAni : MonoBehaviour,IPointerUpHandler,IPointerDownHandler
+{
+    public Transform targetTrans;
+   
+    public Vector3 originValue = new Vector3(1.0f, 1.0f, 1.0f);
+    public Vector3 endValue = new Vector3(0.98f, 0.98f, 0.98f);
+    public Vector3 middleValue = new Vector3(1.1f, 1.1f, 1.1f);
+    public float duration = 0.1f;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (targetTrans==null)
+        {
+            targetTrans = transform;
+        }
+        targetTrans.DOScale(endValue, duration);
+    }
+     
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Sequence s = DOTween.Sequence();
+        s.Append(targetTrans.DOScale(middleValue, duration));
+        s.Append(targetTrans.DOScale(originValue, duration));
+    }
+
+}
